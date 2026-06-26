8f41201ebfcec7540e687f2ae6812dc0b5c82b0a
Felix Paul Kühne <felix@feepk.net>
2026-06-26 19:07:12 +0200

    VLCThumbnailer: rewrite using the libvlc implementation

--- Sources/Tools/VLCMediaThumbnailer.m
+++ Sources/Tools/VLCMediaThumbnailer.m
@@ -2,8 +2,10 @@
  * VLCKit: VLCMediaThumbnailer
  *****************************************************************************
  * Copyright (C) 2010-2012 Pierre d'Herbemont and VideoLAN
+ * Copyright (C) 2026 VLC authors and VideoLAN
  *
  * Authors: Pierre d'Herbemont
+ *          Felix Paul Kühne <fkuehne # videolan.org
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
@@ -26,105 +28,110 @@
 #import <VLCLibVLCBridging.h>
 #import <VLCTime.h>
 #import <VLCLibrary.h>
+#import <VLCEventsHandler.h>
 
 @interface VLCMediaThumbnailer ()
 {
-    NSObject<VLCMediaThumbnailerDelegate>* __weak _thumbnailingDelegate;
+    id<VLCMediaThumbnailerDelegate> __weak _thumbnailingDelegate;
     VLCMedia *_media;
-    void *_mp;
     CGImageRef _thumbnail;
-    void *_data;
-    NSTimer *_parsingTimeoutTimer;
-    NSTimer *_thumbnailingTimeoutTimer;
-
-    CGFloat _thumbnailHeight,_thumbnailWidth;
+    CGFloat _thumbnailHeight, _thumbnailWidth;
     float _snapshotPosition;
-    CGFloat _effectiveThumbnailHeight,_effectiveThumbnailWidth;
-    int _numberOfReceivedFrames;
-    BOOL _shouldRejectFrames;
-
-    VLCLibrary * _library;
+    VLCLibrary *_library;
+    libvlc_parser_t *_parser;
+    VLCEventsHandler *_eventsHandler;
 }
 
-- (void)didFetchThumbnail;
-- (void)notifyDelegate;
-- (void)fetchThumbnail;
-- (void)startFetchingThumbnail;
-
-@property (readonly, assign, nonatomic) void *dataPointer;
-@property (readonly, assign, nonatomic) BOOL shouldRejectFrames;
+- (void)handleThumbnailPicture:(nullable libvlc_picture_t *)picture;
 @end
 
-static void *lock(void *opaque, void **pixels)
-{
-    VLCMediaThumbnailer *thumbnailer = (__bridge VLCMediaThumbnailer *)(opaque);
-
-    *pixels = [thumbnailer dataPointer];
-    assert(*pixels);
-    return NULL;
-}
-
-static const size_t kDefaultImageWidth = 320;
-static const size_t kDefaultImageHeight = 240;
+static const unsigned int kDefaultImageWidth = 320;
+static const unsigned int kDefaultImageHeight = 240;
 static const float kSnapshotPosition = 0.3;
-static const long long kStandardStartTime = 150000;
 
-void unlock(void *opaque, void *picture, void *const *p_pixels)
+static void thumbnailPictureReleaseCallback(void *info, const void *data, size_t size)
 {
-    (void)picture;
-    (void)p_pixels;
+    libvlc_picture_release((libvlc_picture_t *)info);
 }
 
-static void display(void *opaque, void *picture)
+/* Builds a CGImage referencing the picture's buffer without copying it. Consumes
+ * one reference of `picture`: it is released when the returned CGImage is freed,
+ * or immediately if the image can't be created. */
+static CGImageRef VLCThumbnailCGImageCreate(libvlc_picture_t *picture) CF_RETURNS_RETAINED
 {
-    VLCMediaThumbnailer *thumbnailer = (__bridge VLCMediaThumbnailer *)(opaque);
-    assert(!picture);
+    const libvlc_picture_type_t type = libvlc_picture_type(picture);
+    const unsigned int width = libvlc_picture_get_width(picture);
+    const unsigned int height = libvlc_picture_get_height(picture);
+    const unsigned int stride = libvlc_picture_get_stride(picture);
+    size_t size = 0;
+    const unsigned char *buffer = libvlc_picture_get_buffer(picture, &size);
 
-    // We may already have a thumbnail if we are receiving picture after the first one.
-    // Just ignore.
-    if ([thumbnailer thumbnail] || [thumbnailer shouldRejectFrames])
-        return;
+    if ((type != libvlc_picture_Rgba && type != libvlc_picture_Argb)
+        || buffer == NULL || width == 0 || height == 0) {
+        libvlc_picture_release(picture);
+        return NULL;
+    }
 
-    [thumbnailer performSelectorOnMainThread:@selector(didFetchThumbnail) withObject:nil waitUntilDone:YES];
+    CGDataProviderRef provider = CGDataProviderCreateWithData(picture, buffer, size, thumbnailPictureReleaseCallback);
+    if (provider == NULL) {
+        libvlc_picture_release(picture);
+        return NULL;
+    }
+
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateWithName(kCGColorSpaceSRGB);
+    CGImageRef image = CGImageCreate(width, height, 8, 32, stride, colorSpace,
+                                     kCGBitmapByteOrderDefault | kCGImageAlphaNoneSkipLast,
+                                     provider, NULL, false, kCGRenderingIntentDefault);
+    CGColorSpaceRelease(colorSpace);
+    CGDataProviderRelease(provider);
+
+    return image;
 }
+
+static void thumbnailer_on_ended(void *opaque, libvlc_parser_task *task, libvlc_picture_t *picture)
+{
+    @autoreleasepool {
+        VLCEventsHandler *eventsHandler = (__bridge VLCEventsHandler *)opaque;
+        libvlc_picture_t *retained = picture ? libvlc_picture_retain(picture) : NULL;
+        [eventsHandler handleEvent:^(id object) {
+            VLCMediaThumbnailer *thumbnailer = (VLCMediaThumbnailer *)object;
+            [thumbnailer handleThumbnailPicture:retained];
+        }];
+        libvlc_parser_task_release(task);
+    }
+}
+
+static const struct libvlc_thumbnailer_cbs thumbnailer_cbs = {
+    .version = 0,
+    .on_ended = thumbnailer_on_ended,
+};
 
 @implementation VLCMediaThumbnailer
 @synthesize media=_media;
 @synthesize delegate=_thumbnailingDelegate;
 @synthesize thumbnail=_thumbnail;
-@synthesize dataPointer=_data;
 @synthesize thumbnailWidth=_thumbnailWidth;
 @synthesize thumbnailHeight=_thumbnailHeight;
 @synthesize snapshotPosition=_snapshotPosition;
-@synthesize shouldRejectFrames=_shouldRejectFrames;
 
 + (VLCMediaThumbnailer *)thumbnailerWithMedia:(VLCMedia *)media andDelegate:(id<VLCMediaThumbnailerDelegate>)delegate
 {
-    id obj = [[[self class] alloc] init];
-    [obj setMedia:media];
-    [obj setDelegate:delegate];
-    [obj setVLCLibrary: [VLCLibrary sharedLibrary]];
-    return obj;
+    return [self thumbnailerWithMedia:media delegate:delegate andVLCLibrary:nil];
 }
 
 + (VLCMediaThumbnailer *)thumbnailerWithMedia:(VLCMedia *)media delegate:(id<VLCMediaThumbnailerDelegate>)delegate andVLCLibrary:(nullable VLCLibrary *)library
 {
-    id obj = [[[self class] alloc] init];
-    [obj setMedia:media];
-    [obj setDelegate:delegate];
-    if (library)
-        [obj setVLCLibrary: library];
-    else
-        [obj setVLCLibrary: [VLCLibrary sharedLibrary]];
+    VLCMediaThumbnailer *obj = [[self class] new];
+    obj.media = media;
+    obj.delegate = delegate;
+    [obj setVLCLibrary: library ?: [VLCLibrary sharedLibrary]];
     return obj;
 }
 
 - (void)dealloc
 {
-    NSAssert(!_thumbnailingTimeoutTimer, @"Timer not released");
-    NSAssert(!_parsingTimeoutTimer, @"Timer not released");
-    NSAssert(!_data, @"Data not released");
-    NSAssert(!_mp, @"Not properly retained");
+    if (_parser != NULL)
+        libvlc_parser_destroy(_parser);
     if (_thumbnail)
         CGImageRelease(_thumbnail);
 }
@@ -136,218 +143,65 @@ static void display(void *opaque, void *picture)
 
 - (void)fetchThumbnail
 {
-    NSAssert(!_data, @"We are already fetching a thumbnail");
+    NSAssert(_parser == NULL, @"We are already fetching a thumbnail");
 
-    VLCMediaParsedStatus parsedStatus = [_media parsedStatus];
-    if (!(parsedStatus == VLCMediaParsedStatusFailed || parsedStatus == VLCMediaParsedStatusDone)) {
-        VKLog(@"WARNING: media wasn't parsed before thumbnailing");
+    libvlc_media_t *p_media = [_media libVLCMediaDescriptor];
+    if (p_media == NULL)
         return;
-    }
 
-    [self startFetchingThumbnail];
+    const unsigned int imageWidth = _thumbnailWidth > 0 ? (unsigned int)_thumbnailWidth : kDefaultImageWidth;
+    const unsigned int imageHeight = _thumbnailHeight > 0 ? (unsigned int)_thumbnailHeight : kDefaultImageHeight;
+    const double snapshotPosition = _snapshotPosition > 0 ? _snapshotPosition : kSnapshotPosition;
+
+    // remote media may take considerably longer to open than a local file
+    const BOOL isLocal = [_media.url.scheme isEqualToString:@"file"];
+    const struct libvlc_parser_cfg cfg = {
+        .version = 0,
+        .max_parser_threads = 0,
+        .max_thumbnailer_threads = 0,
+        .timeout = (libvlc_time_t)(isLocal ? 10 : 45) * 1000000, // microseconds
+    };
+    _parser = libvlc_parser_new(_library.instance, &cfg);
+    if (_parser == NULL)
+        return;
+
+    _eventsHandler = [VLCEventsHandler handlerWithObject:self configuration:[VLCLibrary sharedEventsConfiguration]];
+
+    const libvlc_thumbnailer_request_t request = {
+        .version = 0,
+        .media = p_media,
+        .width = imageWidth,
+        .height = imageHeight,
+        .crop = false,
+        .type = libvlc_picture_Rgba,
+        .seek = {
+            .type = libvlc_thumbnailer_seek_pos,
+            .value = { .pos = snapshotPosition },
+            .speed = libvlc_media_thumbnail_seek_fast,
+        },
+        .hw_dec = false,
+    };
+
+    libvlc_parser_task *task = libvlc_parser_queue_thumbnailing(_parser, &request, &thumbnailer_cbs, (__bridge void *)_eventsHandler);
+    if (task == NULL)
+        [_thumbnailingDelegate mediaThumbnailerDidTimeOut:self];
 }
 
-- (void)startFetchingThumbnail
+- (void)handleThumbnailPicture:(nullable libvlc_picture_t *)picture
 {
-    unsigned imageWidth = _thumbnailWidth > 0 ? _thumbnailWidth : kDefaultImageWidth;
-    unsigned imageHeight = _thumbnailHeight > 0 ? _thumbnailHeight : kDefaultImageHeight;
-    float snapshotPosition = _snapshotPosition > 0 ? _snapshotPosition : kSnapshotPosition;
-
-    /* optimize rendering if we know what's ahead, if not, well not too bad either */
-    VLCMediaVideoTrack *videoTrack = _media.videoTracks.firstObject.video;
-    if (videoTrack) {
-        int videoHeight = videoTrack.height;
-        int videoWidth = videoTrack.width;
-
-        // Constraining to the aspect ratio of the video.
-        double ratio;
-        if ((double)imageWidth / imageHeight < (double)videoWidth / videoHeight)
-            ratio = (double)imageHeight / videoHeight;
-        else
-            ratio = (double)imageWidth / videoWidth;
-
-        int newWidth = round(videoWidth * ratio);
-        int newHeight = round(videoHeight * ratio);
-
-        imageWidth = newWidth > 0 ? newWidth : imageWidth;
-        imageHeight = newHeight > 0 ? newHeight : imageHeight;
-    }
-
-    _numberOfReceivedFrames = 0;
-    NSAssert(!_shouldRejectFrames, @"Are we still running?");
-
-    _effectiveThumbnailHeight = imageHeight;
-    _effectiveThumbnailWidth = imageWidth;
-    _snapshotPosition = snapshotPosition;
-
-    _data = calloc(1, imageWidth * imageHeight * 4);
-    NSAssert(_data, @"Can't create data");
-
-    NSAssert(!_mp, @"We are already fetching a thumbnail");
-    _mp = libvlc_media_player_new(_library.instance, NULL, NULL);
-    if (_mp == NULL) {
-        NSAssert(0, @"%s: creating the player instance failed", __PRETTY_FUNCTION__);
-        [self endThumbnailing];
-    }
-
-    libvlc_media_add_option([_media libVLCMediaDescriptor], "no-audio");
-    libvlc_media_add_option([_media libVLCMediaDescriptor], "no-spu");
-    libvlc_media_add_option([_media libVLCMediaDescriptor], "avcodec-threads=1");
-    libvlc_media_add_option([_media libVLCMediaDescriptor], "avcodec-skip-idct=4");
-    libvlc_media_add_option([_media libVLCMediaDescriptor], "avcodec-skiploopfilter=3");
-    libvlc_media_add_option([_media libVLCMediaDescriptor], "deinterlace=-1");
-    libvlc_media_add_option([_media libVLCMediaDescriptor], "avi-index=3");
-    libvlc_media_add_option([_media libVLCMediaDescriptor], "codec=avcodec,none");
-
-    libvlc_media_player_set_media(_mp, [_media libVLCMediaDescriptor]);
-    libvlc_video_set_format(_mp, "RGBA", imageWidth, imageHeight, 4 * imageWidth);
-    libvlc_video_set_callbacks(_mp, lock, unlock, display, (__bridge void *)(self));
-    if (snapshotPosition == kSnapshotPosition) {
-        int length = _media.length.intValue;
-        if (length < kStandardStartTime) {
-            VKLog(@"short file detected");
-            if (length > 1000) {
-                VKLog(@"attempting seek to %is", (length * 25 / 100000));
-                libvlc_media_add_option([_media libVLCMediaDescriptor], [[NSString stringWithFormat:@"start-time=%i", (length * 25 / 100000)] UTF8String]);
-            }
-        } else
-            libvlc_media_add_option([_media libVLCMediaDescriptor], [[NSString stringWithFormat:@"start-time=%lli", (kStandardStartTime / 1000)] UTF8String]);
-    }
-    libvlc_media_player_play(_mp);
-
-    NSURL *url = _media.url;
-    NSTimeInterval timeoutDuration = 10;
-    if (![url.scheme isEqualToString:@"file"]) {
-        VKLog(@"media is remote, will wait longer");
-        timeoutDuration = 45;
-    }
-
-    NSAssert(!_thumbnailingTimeoutTimer, @"We already have a timer around");
-    _thumbnailingTimeoutTimer = [NSTimer scheduledTimerWithTimeInterval:timeoutDuration target:self selector:@selector(mediaThumbnailingTimedOut) userInfo:nil repeats:NO];
-}
-
-- (void)mediaParsingTimedOut
-{
-    VKLog(@"WARNING: media thumbnailer media parsing timed out");
-    [_media removeObserver:self forKeyPath:@"parsedStatus"];
-
-    [self startFetchingThumbnail];
-}
-
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
-{
-    if (object == _media && [keyPath isEqualToString:@"parsedStatus"]) {
-        [_parsingTimeoutTimer invalidate];
-        _parsingTimeoutTimer = nil;
-        [_media removeObserver:self forKeyPath:@"parsedStatus"];
-        [self startFetchingThumbnail];
-
-        return;
-    }
-    return [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
-}
-
-- (void)didFetchThumbnail
-{
-    if (_shouldRejectFrames)
-        return;
-
-    // The video thread is blocking on us. Beware not to do too much work.
-    _numberOfReceivedFrames++;
-
-    float position = libvlc_media_player_get_position(_mp);
-    long long length = libvlc_media_player_get_length(_mp);
-
-    // Make sure we are getting the right frame
-    if (position < self.snapshotPosition && _numberOfReceivedFrames < 2) {
-        libvlc_media_player_set_position(_mp, self.snapshotPosition, YES);
-        return;
-    }
-    if ((length < kStandardStartTime * 2 && _numberOfReceivedFrames < 5) && self.snapshotPosition == kSnapshotPosition) {
-        libvlc_media_player_set_position(_mp, kSnapshotPosition, YES);
-        return;
-    }
-    if ((position <= 0.05 && _numberOfReceivedFrames < 8) && length > 1000) {
-        // Arbitrary choice to work around broken files.
-        libvlc_media_player_set_position(_mp, kSnapshotPosition, YES);
-        return;
-    }
-    // it isn't always best what comes first
-    if (_numberOfReceivedFrames < 4) {
+    CGImageRef image = picture ? VLCThumbnailCGImageCreate(picture) : NULL;
+    if (image == NULL) {
+        [_thumbnailingDelegate mediaThumbnailerDidTimeOut:self];
         return;
     }
 
-    NSAssert(_data, @"We have no data");
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    const CGFloat width = _effectiveThumbnailWidth;
-    const CGFloat height = _effectiveThumbnailHeight;
-    const CGFloat pitch = 4 * width;
-    CGContextRef bitmap = CGBitmapContextCreate(_data,
-                                                width,
-                                                height,
-                                                8,
-                                                pitch,
-                                                colorSpace,
-                                                kCGImageAlphaNoneSkipLast);
-
-    CGColorSpaceRelease(colorSpace);
-    NSAssert(bitmap, @"Can't create bitmap");
-
-    // Create the thumbnail image
-    //NSAssert(!_thumbnail, @"We already have a thumbnail");
     if (_thumbnail)
         CGImageRelease(_thumbnail);
-    _thumbnail = CGBitmapContextCreateImage(bitmap);
-    _thumbnailWidth = _effectiveThumbnailWidth;
-    _thumbnailHeight = _effectiveThumbnailHeight;
+    _thumbnail = image;
+    _thumbnailWidth = CGImageGetWidth(image);
+    _thumbnailHeight = CGImageGetHeight(image);
 
-    // Put a new context there.
-    CGContextRelease(bitmap);
-
-    // Make sure we don't block the video thread now
-    [self performSelector:@selector(notifyDelegate) withObject:nil afterDelay:0];
-}
-
-- (void)stopAsync
-{
-    if (_mp) {
-        libvlc_media_player_stop_async(_mp);
-        libvlc_media_player_release(_mp);
-        _mp = NULL;
-    }
-
-    // Now release data
-    if (_data)
-        free(_data);
-    _data = NULL;
-
-    _shouldRejectFrames = NO;
-}
-
-- (void)endThumbnailing
-{
-    _shouldRejectFrames = YES;
-
-    [_thumbnailingTimeoutTimer invalidate];
-    _thumbnailingTimeoutTimer = nil;
-
-    [self performSelectorInBackground:@selector(stopAsync) withObject:nil];
-}
-
-- (void)notifyDelegate
-{
-    [self endThumbnailing];
-
-    // Call delegate
     [_thumbnailingDelegate mediaThumbnailer:self didFinishThumbnail:_thumbnail];
 }
 
-- (void)mediaThumbnailingTimedOut
-{
-    VKLog(@"WARNING: media thumbnailer media thumbnailing timed out");
-    [self endThumbnailing];
-
-    // Call delegate
-    [_thumbnailingDelegate mediaThumbnailerDidTimeOut:self];
-}
 @end
